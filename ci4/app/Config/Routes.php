0ea31a521f0962756278775897d73ef813e5e19e
Roman Nikko Albania <rbalbania@student.apc.edu.ph>
2024-02-28 14:50:50 +0800

    added code for ci4tutorial news insert

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -6,6 +6,8 @@ use App\Controllers\News; // Add this line
 use App\Controllers\Pages;
 
 $routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/new', [News::class, 'new']); // Add this line
+$routes->post('news', [News::class, 'create']); // Add this line
 $routes->get('news/(:segment)', [News::class, 'show']); // Add this line
 
 $routes->get('pages', [Pages::class, 'index']);
--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -17,6 +17,7 @@ class NewsModel extends Model
     }
     
     protected $table = 'news';
+    protected $allowedFields = ['title', 'slug', 'body'];
 }
 
 
